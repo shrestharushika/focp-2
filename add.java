04db001e4bfab6901e764e92a00ae62db62aca40
rushika <shrestharushika@gmail.com>
2019-01-10 13:23:47 +0530

    added rushika file

---
+++ add.java
@@ -0,0 +1,10 @@
+class add
+{ 
+  public static void main(String[] args)
+  {
+    int x=7;
+int y=4;
+ int z=x+y;
+  System.out.println("sum="+z);
+   }
+}
---
+++ char.java
@@ -0,0 +1,9 @@
+ class character
+{
+   public static void main(String[] args)
+  {
+   char c='A';
+ int ascii=c;
+  System.out.println("the value ascii"+c+"is"+ascii);
+  }
+}
